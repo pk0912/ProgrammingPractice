4e70de8490c3f745812cef994a56c83bc35ac227
Praveen Kumar <prvnkr.pk@gmail.com>
2020-08-19 18:40:20 +0530

    unnecessary code removed

--- arrays/medium/contiguous_subarray_with_max_sum_kadane.py
+++ arrays/medium/contiguous_subarray_with_max_sum_kadane.py
@@ -7,9 +7,8 @@ for t in range(T):
     a = input().split()
     A.append([int(i.strip()) for i in a])
 for i in range(len(N)):
-    start = 0
     current_sum = 0
-    max_sum = A[i][start]
+    max_sum = A[i][0]
     j = 1
     while j <= N[i]:
         if current_sum + A[i][j - 1] >= 0:
@@ -17,7 +16,6 @@ for i in range(len(N)):
             if current_sum > max_sum:
                 max_sum = current_sum
         else:
-            start = j
             current_sum = 0
         j += 1
     print(max_sum)
