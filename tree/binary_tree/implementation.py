e456fd04e45d2c127c2283ae14f5ad97383fb79e
Praveen Kumar <prvnkr.pk@gmail.com>
2021-04-19 07:30:37 +0530

    preorder, postorder traversal for a binary tree done; inorder without recursion also done

--- tree/binary_tree/implementation.py
+++ tree/binary_tree/implementation.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -8,12 +11,47 @@ class Node:
         return f"{self.data}"
 
 
-def inorder(node):
+def inorder_without_recursion(node):
+    stack = deque()
+    while node is not None or len(stack) != 0:
+        if node is not None:
+            stack.appendleft(node)
+            node = node.left
+        else:
+            popped_item = stack.popleft()
+            print(popped_item.data, end=" ")
+            node = popped_item.right
+
+
+def inorder_with_recursion(node):
     if node is None:
         return
     inorder(node.left)
     print(node, end=" ")
     inorder(node.right)
+
+
+def inorder(node, recursion=True):
+    if recursion:
+        inorder_with_recursion(node)
+    else:
+        inorder_without_recursion(node)
+
+
+def preorder(node):
+    if node is None:
+        return
+    print(node, end=" ")
+    preorder(node.left)
+    preorder(node.right)
+
+
+def postorder(node):
+    if node is None:
+        return
+    postorder(node.left)
+    postorder(node.right)
+    print(node, end=" ")
 
 
 def insert(node, val):
@@ -46,7 +84,22 @@ if __name__ == '__main__':
     root.right.left = Node(6)
     root.right.right = Node(7)
     # root = None
+    print("Inorder using recursion : ", end="")
     inorder(root)
-    root = insert(root, 8)
     print()
+    print("Inorder without recursion : ", end="")
+    inorder(root, recursion=False)
+    print()
+    root = insert(root, 8)
+    print("Inorder using recursion : ", end="")
     inorder(root)
+    print()
+    print("Inorder without recursion : ", end="")
+    inorder(root, recursion=False)
+    print()
+    print("Preorder : ", end="")
+    preorder(root)
+    print()
+    print("Postorder : ", end="")
+    postorder(root)
+    print()
