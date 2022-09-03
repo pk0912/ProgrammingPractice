495dc8e009f3a8382893d37d31b67cff7f052b9b
Praveen Kumar <prvnkr.pk@gmail.com>
2022-09-03 22:47:25 +0530

    heap : added kth largest element in list

--- leetcode/heap/kth_largest_element_in_list.py
+++ leetcode/heap/kth_largest_element_in_list.py
@@ -16,5 +16,5 @@ class Solution:
 
 
 if __name__ == '__main__':
-    # print(Solution().findKthLargest([3,2,1,5,6,4], 2))
+    print(Solution().findKthLargest([3,2,1,5,6,4], 2))
     print(Solution().findKthLargest([3,2,3,1,2,4,5,5,6], 4))
