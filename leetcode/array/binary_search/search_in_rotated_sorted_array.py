8756b56b2549e48a878287c67bfaf6c61eac4e7f
Praveen Kumar <prvnkr.pk@gmail.com>
2022-10-15 15:25:05 +0530

    Binary Search : Search in rotated sorted array done

---
+++ leetcode/array/binary_search/search_in_rotated_sorted_array.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+        while low <= high:
+            mid = (low + high) >> 1
+            if nums[mid] == target:
+                return mid
+            if nums[low] <= nums[mid]:
+                if nums[low] <= target < nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:
+                if nums[mid] < target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+        return -1
+
+
+if __name__ == '__main__':
+    print(Solution().search([4, 5, 6, 7, 0, 1, 2], 0))
+    print(Solution().search([4, 5, 6, 7, 0, 1, 2], 3))
+    print(Solution().search([4, 5, 6, 7, 0, 1, 2], 4))
+    print(Solution().search([3, 1], 1))
